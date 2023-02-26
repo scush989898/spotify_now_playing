604685390dee16c6f6b0f56949037787b09d69be
Antoniel Mariano <antonielcamposm@gmail.com>
2023-02-26 17:48:08 -0300

    Feat: added logic to create or write file text

---
+++ source/FileIO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace source
+{
+    public class FileIO
+    {
+        static string path = "C:\\Users\\anton\\Desktop\\Projetos\\music_playing.txt";
+        public static void CheckFile()
+        {
+            if (!File.Exists(path))
+            {
+                CreateFile();
+            }
+            if (File.Exists(path))
+            {
+                ReadFile();
+            }
+        }
+
+        public static void CreateFile()
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+
+                Process[] localByName = Process.GetProcessesByName("spotify");
+                foreach (Process process in localByName)
+                {
+                    if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                    {
+                        sw.WriteLine(process.MainWindowTitle);
+                    }
+                }
+            }
+        }
+
+        public static void ReadFile()
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+
+                Process[] localByName = Process.GetProcessesByName("spotify");
+                foreach (Process process in localByName)
+                {
+                    if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                    {
+                        sw.WriteLine(process.MainWindowTitle);
+                    }
+                }
+            }
+        }
+    }
+}
--- source/Program.cs
+++ source/Program.cs
@@ -10,6 +10,7 @@ namespace source
     {
         public static void Main(string[] args)
         {
+            FileIO.CheckFile();
         }
     }
 }
